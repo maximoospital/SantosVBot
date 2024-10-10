4c25f7dedaaeb9389b5bfeddcaefea1fc20183dc
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-10-10 19:45:20 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -9,7 +9,7 @@ const retweet = async () => {
   console.log('Directory contents:', fs.readdirSync('.'));
   
   const path = require('path');
-  const mediaId = await client.v1.uploadMedia(path.join(__dirname, 'test.jpg'));
+  const mediaId = await client.v1.uploadMedia(path.join(__dirname, '/src/test.jpg'));
   const createdTweet = await client.v2.tweet('Hello!', { media_ids: mediaId });
   console.log('Tweet', createdTweet.id, ':', createdTweet.text);
 
