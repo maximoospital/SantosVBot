8319b267b5ca2d93ec5df4eee066ef2e7ccfd921
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-10-10 16:53:18 -0300

    u p s

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 const { TwitterApi } = require('twitter-api-v2');
 const config = require("./config");
-const TwitterBot = new TwitterApi(config);
+const client = new TwitterApi(config);
 
 // API
 console.log("Tweeting...")
@@ -9,7 +9,7 @@ const retweet = async () => {
   const mediaId = await client.v1.uploadMedia('test.jpg');
   const createdTweet = await client.v2.tweet('Hello!', { media_ids: mediaId });
   console.log('Tweet', createdTweet.id, ':', createdTweet.text);
-  
+
 };
 
 retweet();
