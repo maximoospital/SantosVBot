b68703a20977a22f03b6aa39ce79435c15fae7f9
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-10-10 16:02:27 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -3,9 +3,10 @@ const config = require("./config");
 const TwitterBot = new Twitter(config);
 
 // API
-
+console.log("Tweeting...")
 const retweet = () => {
   TwitterBot.post('statuses/update', {status: 'Test tweet'}, function(error, tweet, response) {
+    console.log(response);
     if (!error) {
       console.log(tweet);
     }
