3601ea6e4db9fc969188fc9b142614fc65c81096
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-10-10 16:48:14 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -5,11 +5,10 @@ const TwitterBot = new TwitterApi(config);
 // API
 console.log("Tweeting...")
 const retweet = async () => {
-  const createdTweet = await TwitterBot.v1.tweet('twitter-api-v2 is awesome!', {
-    lat: 1.23,
-    long: -13.392,
+  const { data: createdTweet } = await TwitterBot.v2.tweet('Test!', {
+    poll: { duration_minutes: 120, options: ['Test 1', 'Test 2!'] },
   });
-  console.log('Tweet', createdTweet.id_str, ':', createdTweet.full_text);
+  console.log('Tweet', createdTweet.id, ':', createdTweet.text);
 };
 
 retweet();
