51d1379d27136f161ccd52bfccbb9756d808b569
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-10-10 16:18:39 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@
 require("dotenv").config();
 
 module.exports = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
+  consumer_key: process.env.CONSUMER_TOKEN,
+  consumer_secret: process.env.CONSUMER_TOKEN_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
