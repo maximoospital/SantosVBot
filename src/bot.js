e7378104e60cd47b80fbfc403f8eaffb2299f5b3
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-10-10 16:46:28 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -5,7 +5,7 @@ const TwitterBot = new TwitterApi(config);
 // API
 console.log("Tweeting...")
 const retweet = async () => {
-  const createdTweet = await client.v1.tweet('twitter-api-v2 is awesome!', {
+  const createdTweet = await TwitterBot.v1.tweet('twitter-api-v2 is awesome!', {
     lat: 1.23,
     long: -13.392,
   });
