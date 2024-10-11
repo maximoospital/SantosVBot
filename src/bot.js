145c6fdf558c08fa4995772e3a6dd2e45f4a5e85
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2024-10-10 21:50:02 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -9,9 +9,7 @@ const retweet = async () => {
   const path = require('path');
   const mediaId = await client.v1.uploadMedia(path.join(__dirname, '/test.jpg'));
   const createdTweet = await client.v2.tweet({ text: 'Twitter is a fantastic social network. Look at this:', media: { media_ids: [mediaId] } });
-  
-  console.log('Tweet', createdTweet.id, ':', createdTweet.text);
-
+  console.log(createdTweet);
 };
 
 retweet();
